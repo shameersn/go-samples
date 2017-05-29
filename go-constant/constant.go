f6eb14930118275728dde33c61035e8ac026d48b
shameersn <shameersalnaz@gmail.com>
2017-05-29 17:54:01 +0530

    go Constants

--- go-constant/constant.go
+++ go-constant/constant.go
@@ -6,8 +6,25 @@ import (
 
 const p string = "Hello from const outside"
 
+const (
+	run = "Go to your home"
+	age = 23
+)
+
+// const using iota
+const (
+	A = iota
+	B
+	C
+)
+
 func main() {
 	const Q int = 42
 	fmt.Println(Q)
 	fmt.Println(p)
+	fmt.Println(run)
+	fmt.Println(age)
+	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(C)
 }
